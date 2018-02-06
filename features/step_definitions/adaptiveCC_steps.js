5be2ae7fdb9fc78827d3ab021bbcb47c7d5e46db
Fin Kingma <fkingma@xebia.com>
2018-02-06 15:21:11 +0100

    .

--- features/step_definitions/adaptiveCC_steps.js
+++ features/step_definitions/adaptiveCC_steps.js
@@ -1,29 +1,26 @@
 var assert = require('chai').assert
 
 module.exports = function () {
-  this.Given(/^Jim is driving at (\d+)mph$/, (speed, done) => {
-    var sinon = require('sinon')
-    this.clock = sinon.useFakeTimers()
-
+  this.Given(/^Jim is driving at (\d+)mph$/, function (speed, done) {
     var Car = require('../../src/modules/car.js')
     this.jim = new Car()
     this.jim.setSpeed(speed)
     done()
   })
   
-  this.Given(/^John is driving at (\d+)mph$/, (speed, done) => {
+  this.Given(/^John is driving at (\d+)mph$/, function (speed, done) {
     var Car = require('../../src/modules/car.js')
     this.john = new Car(this.jim)
     this.john.setSpeed(speed)
     done()
   })
 
-  this.When(/^John's car approaches Jims'$/, (done) => {
+  this.When(/^John's car approaches Jims'$/, function (done) {
     this.clock.tick(2000)
     done()
   })
 
-  this.Then(/^John's car adjusts its speed to Jim's car$/, (done) => {
+  this.Then(/^John's car adjusts its speed to Jim's car$/, function (done) {
     assert.equal(this.john.speed, this.jim.speed, 'johns car has not been adjusted')
     done()
   })
--- features/support/World.js
+++ features/support/World.js
@@ -1,7 +1,10 @@
-function World () {
+var sinon = require('sinon')
+var clock = sinon.useFakeTimers()
 
+function World () {
+  this.clock = clock
 }
 
-module.exports = () => {
+module.exports = function () {
   this.World = World
 }
