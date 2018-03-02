d4e33cd442bd05a8d6f6015a601dc2e58d1faf39
finkingma <finkingma@gmail.com>
2018-03-02 16:58:05 +0100

    .

--- features/AdaptiveCC.feature
+++ features/AdaptiveCC.feature
@@ -1,7 +1,10 @@
 Feature: Adaptive Cruise Control
-  As a Driver
+  As a driver
   I want my car to adjust its speed to the car in front of me
-  To help me anticipate strange behavior that could result in a crash
+  So that I can use my cruise control in traffic
 
 Scenario: 
-
+  Given John drives at 60 mph
+  And William drives at 70 mph
+  When William nears John
+  Then John's car will adjust its speed to 60 mph
--- features/step_definitions/adaptiveCC_steps.js
+++ features/step_definitions/adaptiveCC_steps.js
@@ -1,6 +1,28 @@
-// var assert = require('chai').assert
-// var Car = require('../../src/modules/car.js')
+var assert = require('chai').assert
+var Car = require('../../src/modules/car.js')
 
 module.exports = function () {
-  
+    this.Given(/^John drives at (\d+) mph$/, function (speed, callback) {
+        this.John = new Car()
+        this.John.setSpeed(speed)
+        callback()
+    });
+
+    this.Given(/^William drives at (\d+) mph$/, function (speed, callback) {
+        this.William = new Car()
+        this.William.setSpeed(speed)
+        callback()
+    });
+
+    this.When(/^William nears John$/, function (callback) {
+        this.John.setPosition(1000)
+        this.William.setPosition(900)
+        callback()
+    });
+
+    this.Then(/^John's car will adjust its speed to (\d+) mph$/, function (expectedSpeed, callback) {
+        this.clock.tick(2000)
+        assert(this.John.speed === expectedSpeed, 'johns car does not drive with expected speed')
+        callback()
+    });
 }
